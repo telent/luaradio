12ba3f4041f7eb15a9adb576d07862b0c1cbcde7
Daniel Barlow <dan@telent.net>
2020-02-20 22:59:38 +0000

    cleanup the signal better

--- finding-dore.lua
+++ finding-dore.lua
@@ -1,24 +1,37 @@
 local radio = require('radio')
 
+function basename(str)
+    local name = string.gsub(str, "(.*/)(.*)", "%2")
+    return name
+end
+
 top = radio.CompositeBlock()
 
+local filename = basename(arg[1])
 local src = radio.IQFileSource(arg[1],'u8', 1e6)
-local lpf = radio.LowpassFilterBlock(25, 800)
+local lpf = radio.LowpassFilterBlock(5, 500)
 local slicer = radio.SlicerBlock(0)
 local clock_recoverer = radio.ZeroCrossingClockRecoveryBlock(4800)
+local delayed_clock = radio.DelayBlock(64)
 local mag = radio.ComplexMagnitudeBlock()
 local shift = radio.AddConstantBlock(-0.5)
+local clip = radio.ClipBlock(-0.3, 0.2)
 local sampler = radio.SamplerBlock()
 
 top:connect(src, lpf)
 top:connect(lpf, mag)
 top:connect(mag, shift)
+top:connect(shift, clip)
+top:connect(clip, radio.WAVFileSink('/tmp/'..filename..'_0.wav', 1))
+	    
+top:connect(clip, 'out', clock_recoverer, 'in')
+top:connect(clip, 'out', sampler, 'data')
+top:connect(clock_recoverer, delayed_clock)
 
-top:connect(shift, 'out', clock_recoverer, 'in')
-top:connect(shift, 'out', sampler, 'data')
-top:connect(clock_recoverer, 'out', sampler, 'clock')
+top:connect(delayed_clock, 'out', sampler, 'clock')
+top:connect(sampler, 'out',
+            radio.WAVFileSink('/tmp/'..filename..'_1.wav', 1), 'in')
 top:connect(sampler,  slicer)
-top:connect(sampler, 'out', radio.WAVFileSink('/tmp/1.wav', 1), 'in')
 decoder = radio.BiphaseMarkDecoderBlock()
 top:connect(slicer,  decoder)
 --top:connect(decoder, radio.PrintSink())
